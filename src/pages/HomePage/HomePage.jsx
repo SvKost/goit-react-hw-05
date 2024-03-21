71ffa812ab5ce96dc76534da7310d51b435a7bd1
SvKost <skost0634610557@gmail.com>
2024-03-21 23:06:47 +0200

    Added NavLink to HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,20 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import MovieDetailsPage from "../MovieDetailsPage/MovieDetailsPage";
+import NotFoundPage from "../NotFoundPage/NotFoundPage";
+
 const HomePage = ({ movies }) => {
   return (
     <div>
       <h1>Tranding Today</h1>
-
       <ul>
         {movies.map((movie) => {
-          return <li key={movie.id}>{movie.title}</li>;
+          return (
+            <li key={movie.id}>
+              <NavLink to="/movieDetails/${movie.id}" key={movie.id}>
+                {movie.title}
+              </NavLink>
+            </li>
+          );
         })}
       </ul>
     </div>
